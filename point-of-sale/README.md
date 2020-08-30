89480b94ca0ff72636b36207a637de7e7364de89
demi.jiang <demi.jiang@afterpay.com>
2020-08-30 16:23:20 +1000

    Sale decides to display the price

---
+++ point-of-sale/README.md
@@ -0,0 +1,12 @@
+1. Write a test list 
+2. TDD a solution (http://wiki.c2.com/?TestDrivenDevelopment)
+3. Publish your solution + commit => GitHub
+4. Pick someone else’s code to review 
+
+Input is the barcode 
+Output it is the product price 
+
+Test List 
+Product found 
+Product not found 
+Invalid barcode empty barcode null barcode 
--- point-of-sale/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ point-of-sale/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -1,32 +1,55 @@
 package ca.jbrains.pos.test;
 
+import org.junit.Ignore;
 import org.junit.Test;
-
-import java.awt.*;
-
 import static org.junit.Assert.assertEquals;
 
 public class SellOneItemTest {
+
     @Test
-    public void productFound() throws Exception {
+    public void productFound() {
         final Display display = new Display();
-        final Sale sale = new Sale();
+        final Sale sale = new Sale(display);
 
         sale.onBarcode("12345");
 
         assertEquals("$7.95", display.getText());
     }
 
+    @Test
+    @Ignore("Refactoring...")
+    public void anotherProductFound() {
+        final Display display = new Display();
+        final Sale sale = new Sale(display);
+
+        sale.onBarcode("23456");
+
+        assertEquals("$12.5", display.getText());
+    }
+
     public static class Display{
 
+        private String text;
+
         public String getText() {
-            return "$7.95" ;
+            text = "$7.95";
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
         }
     }
 
     public static class Sale{
-        public void onBarcode(String barcode){
+        private Display display;
 
+        public Sale(Display display) {
+            this.display = display;
+        }
+
+        public void onBarcode(String barcode){
+            display.setText("$7.95");
         }
     }
 }
