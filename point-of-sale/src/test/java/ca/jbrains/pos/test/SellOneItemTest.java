9c4a426c1184fdc86e1cde6839350372cce35bfa
demi.jiang <demi.jiang@afterpay.com>
2020-08-30 16:40:39 +1000

    We can handle any missing barcode

--- point-of-sale/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ point-of-sale/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -1,6 +1,7 @@
 package ca.jbrains.pos.test;
 
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class SellOneItemTest {
@@ -35,7 +36,7 @@ public class SellOneItemTest {
         assertEquals("Product not found for 99999", display.getText());
     }
 
-    public static class Display{
+    public static class Display {
         private String text;
 
         public String getText() {
@@ -47,20 +48,21 @@ public class SellOneItemTest {
         }
     }
 
-    public static class Sale{
+    public static class Sale {
         private Display display;
 
         public Sale(Display display) {
             this.display = display;
         }
 
-        public void onBarcode(String barcode){
-            if(barcode.equals("12345"))
+        public void onBarcode(String barcode) {
+            if (barcode.equals("12345"))
                 display.setText("$7.95");
-            else if(barcode.equals("23456"))
-            display.setText("$12.50");
+            else if (barcode.equals("23456"))
+                display.setText("$12.50");
             else
-                display.setText("Product not found for 99999");
+                display.setText("Product not found for " +
+                        barcode);
         }
     }
 }
